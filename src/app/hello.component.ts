ecc6f5a166dc43a01b8c0065c0c7d51e4a66ec1d
Pnayak10 <35223661+Pnayak10@users.noreply.github.com>
2022-10-16 00:03:39 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -32,12 +32,19 @@ export class HelloComponent implements OnInit {
   }
 
   serachDuplicateData(inputData, item): any {
-    const LetterBigramSplit1 = getBigram(word),
-          LetterBigramSplit2 = getBigram(word2);
+    const LetterBigramSplit1 = this.getLetterBigramSplit(inputData),
+      LetterBigramSplit2 = this.getLetterBigramSplit(item);
     let similar = [];
-    // for (let i = 0; i < bigraml. length; i++) { if (bigram2.indexOf(bigraml[i]) > -1) {
-    // similar.push(bigraml[i]);
-    // return similar. length / Math.max(bigraml. length, bigram2. length);
+    for (let i = 0; i < LetterBigramSplit1.length; i++) {
+      if (LetterBigramSplit2.indexOf(LetterBigramSplit1[i]) > -1) {
+        similar.push(LetterBigramSplit1[i]);
+        return (
+          similar.length /
+            Math.max(LetterBigramSplit1.length, LetterBigramSplit2.length) >
+          0.5
+        );
+      }
+    }
   }
 
   searchTransposeData(inputData, item): any {
